3ed7fced5233c3ef09fd2716ccd0f5678508ce85
keita.nakamura <knm22mukk@gmail.com>
2021-03-20 07:08:51 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = '新規登録が完了しました'
+      redirect_to root_path
+    else
+      flash.now[:danger] = '新規登録に失敗しました'
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    
+      if @user.update
+        flash[:success] = 'ユーザー情報を編集しました'
+        redirect_to @user
+      else
+        flash.now[:danger] = 'ユーザーの編集に失敗しました'
+        render :edit
+      end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = 'ユーザーを削除しました。'
+    redirect_to root_path
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
